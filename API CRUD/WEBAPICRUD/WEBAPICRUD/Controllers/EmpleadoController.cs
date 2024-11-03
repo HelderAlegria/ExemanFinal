4030fb4f8afc878f71ccdbfb06b6e889a997052e
HelderAlegria <halegriap@miumg.edu.gt>
2024-11-03 08:47:34 -0600

    examen

---
+++ API CRUD/WEBAPICRUD/WEBAPICRUD/Controllers/EmpleadoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using WEBAPICRUD.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.Design;
+
+namespace WEBAPICRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpleadoController : ControllerBase
+    {
+        private readonly PruebaContext dbContext;
+
+        public EmpleadoController(PruebaContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Get()
+        {
+            var listaEmpleado = await dbContext.Empleados.ToListAsync();
+            return StatusCode(StatusCodes.Status200OK, listaEmpleado);
+        }
+
+
+
+        [HttpPost]
+        [Route("Nuevo")]
+        public async Task<IActionResult> Nuevo([FromBody] Empleado objeto)
+        {
+            await dbContext.Empleados.AddAsync(objeto);
+            await dbContext.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok"});
+        }
+
+
+    }
+}
---
+++ API CRUD/WEBAPICRUD/WEBAPICRUD/Models/Empleado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WEBAPICRUD.Models;
+
+public partial class Empleado
+{
+    public int IdEmpleado { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Apellido { get; set; }
+
+    public string? genero { get; set; }
+
+    public string? fechaNacimeinto { get; set; }
+
+    public string? estado { get; set; }
+
+    public string? direccion { get; set; }
+
+    public string? fechaCreacion { get; set; }
+
+    public string? fechaActualizacion { get; set; }
+
+    public int? usuarioCreo { get; set; }
+
+    public string? estadoInformacion { get; set; }
+}
